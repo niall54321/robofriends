d4e99ecb980acd358f9a570ba4f1285b7172e259
Niall Whelan <niallwhelan2010@hotmail.com>
2021-06-28 09:53:25 +0100

    Converting to React Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,46 +1,41 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
-class App extends Component {
-	constructor(){
-		super()
-		this.state={
-			robots: [],
-			searchField: ''
-		}
-	}
+function App() {
 
-	componentDidMount(){
+	const [robots, setRobots] = useState([]);
+	const [searchField, setSearchField] = useState('');
+
+	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response=>response.json())
-		.then(users=>this.setState({robots: users}));
+		.then(users=>setRobots(users));
+	},[])
 		
-	}
-	onSearchChange = (event) =>{
-		this.setState({searchField: event.target.value})
+	const onSearchChange = (event) =>{
+		setSearchField(event.target.value)
 	}
 
-	render(){
-		const {robots, searchField} = this.state;
-		const filteredRobots= robots.filter(robot =>{
-			return robot.name.toLowerCase().includes(searchField.toLowerCase())
-		})
-		if (!robots.length) {
-			return <h1 className='tc f1'>Loading...</h1>
-		} else {
-			return (
-				<div  className='tc'>
-					<h1 className='f1'>RoboFriends</h1>
-					<SearchBox searchChange={this.onSearchChange}/>
-					<Scroll>
-						<CardList robots={filteredRobots}/>
-					</Scroll>
-				</div>
-			);			
-		}
+	const filteredRobots= robots.filter(robot =>{
+		return robot.name.toLowerCase().includes(searchField.toLowerCase())
+	})
+
+	if (!robots.length) {
+		return <h1 className='tc f1'>Loading...</h1>
+	} else {
+		return (
+			<div  className='tc'>
+				<h1 className='f1'>RoboFriends</h1>
+				<SearchBox searchChange={onSearchChange}/>
+				<Scroll>
+					<CardList robots={filteredRobots}/>
+				</Scroll>
+			</div>
+		);			
 	}
 }
+
 export default App
